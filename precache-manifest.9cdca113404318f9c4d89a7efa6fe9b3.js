817da9475dc605f7eda22e33af9bbe57ab6eb663
Denis Castanheira <deniscastanheira@cvccorp.com.br>
2019-10-24 20:28:49 -0300

    Updates

---
+++ precache-manifest.9cdca113404318f9c4d89a7efa6fe9b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "327e38d1b85ff099b303e0597fcb7295",
+    "url": "/pushercoins/index.html"
+  },
+  {
+    "revision": "2c94566ef9db99f1d5f4",
+    "url": "/pushercoins/static/css/main.183060c6.chunk.css"
+  },
+  {
+    "revision": "070887f0cd45ca567860",
+    "url": "/pushercoins/static/js/2.0c72f2e0.chunk.js"
+  },
+  {
+    "revision": "2c94566ef9db99f1d5f4",
+    "url": "/pushercoins/static/js/main.e1c79467.chunk.js"
+  },
+  {
+    "revision": "2c297de4031256fcb54b",
+    "url": "/pushercoins/static/js/runtime-main.0a0ba351.js"
+  }
+]);
